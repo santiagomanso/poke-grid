c3d283297390d57ff49b322bf3cf9fe35360217a
santiagomanso <santiagomanso@web.de>
2023-05-26 22:30:42 +0200

    📝 added language context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,14 +11,17 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 
 import Layout from "~/components/Layout";
+import { LanguageProvider } from "~/context/LanguageContext";
 
 library.add(fas);
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <LanguageProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </LanguageProvider>
   );
 };
 
