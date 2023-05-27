704e4468e69a1136228dce7831a62aadfed175aa
santiagomanso <santiagomanso@web.de>
2023-05-27 18:43:41 +0200

    📝 🔧 added interfaces / reworked existing ones

--- src/interfaces/interfaces.ts
+++ src/interfaces/interfaces.ts
@@ -1,3 +1,4 @@
+import { type IconDefinition } from "@fortawesome/free-solid-svg-icons";
 import { type strstrstrToVoid } from "~/types/types";
 
 //components with children
@@ -5,14 +6,28 @@ export interface children {
   children: React.ReactNode;
 }
 
-//LANGUAGE switcher
+//navigation
+export interface navItems {
+  id: number;
+  icon: IconDefinition | boolean;
+  text: {
+    [index: string]: string;
+    en: string;
+    es: string;
+    de: string;
+  };
+  href: string;
+  specialItem: boolean;
+}
+
+//LANGUAGE
 export interface languageContextI {
   language: string;
-  text: object | undefined;
+  text: Dictionary;
   flag: string;
   name: string;
   setLanguage?: (arg: string) => void;
-  setText?: (arg: object) => void;
+  setText?: (arg: Dictionary) => void;
   setFlag?: (arg: string) => void;
   setName?: (arg: string) => void;
   changeLanguage: strstrstrToVoid;
@@ -30,8 +45,24 @@ export interface Country {
   language: "en" | "de" | "es";
 }
 
+export interface LanguageText {
+  home: string;
+  howToPlay: string;
+  shopNow: string;
+  players: string;
+  login: string;
+  cart: string;
+  myProfile: string;
+  logout: string;
+  welcome: string;
+  greenEnergy: string;
+  description: string;
+  toTheGrid: string;
+  // Add other translation keys and their respective types if needed
+}
+
 export interface Dictionary {
-  [index: string]: object;
+  [index: string]: LanguageText;
 }
 
 // THEME SWITCHER
