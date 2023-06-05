2657f7c233a868c355e32ca0a30b43b0237ea7c0
santiagomanso <santiagomanso@web.de>
2023-06-05 08:39:00 +0200

    🗃️ 🔨 created sample pokemons data

---
+++ src/utils/SamplePokemons.ts
@@ -0,0 +1,46 @@
+import { type pokemons } from "~/interfaces/pokemonInterfaces";
+
+export const SamplePokemons: pokemons = [
+  {
+    id: 1,
+    attack: "impact trueno",
+    attackPower: 30,
+    cost: 0,
+    name: "pikachu",
+    image:
+      "https://www.pngall.com/wp-content/uploads/5/Cute-Pikachu-Transparent.png",
+  },
+  {
+    id: 2,
+    attack: "Thunder Shock",
+    attackPower: 50,
+    cost: 20,
+    name: "magnemite",
+    image: "https://www.pngmart.com/files/22/Magnemite-Pokemon-PNG-Clipart.png",
+  },
+  {
+    id: 3,
+    attack: "Shock Wave",
+    attackPower: 70,
+    cost: 60,
+    name: "electabuzz",
+    image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/125.png",
+  },
+  {
+    id: 4,
+    attack: "Thunder Wave",
+    attackPower: 90,
+    cost: 80,
+    name: "jolteon",
+    image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/135.png",
+  },
+  {
+    id: 5,
+    attack: "Zap Cannon",
+    attackPower: 120,
+    cost: 100,
+    name: "zapdos",
+    image:
+      "https://www.pngmart.com/files/22/Zapdos-Pokemon-Transparent-PNG.png",
+  },
+];
