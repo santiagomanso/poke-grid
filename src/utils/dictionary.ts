1f132682ec4653d6787847015a30aadf1926348f
santiagomanso <santiagomanso@web.de>
2023-07-26 21:22:02 +0200

    📝 reworked diccionary

--- src/utils/dictionary.ts
+++ src/utils/dictionary.ts
@@ -17,6 +17,7 @@ export const dictionary: Dictionary = {
     greenEnergy: "Green energy by pokemons",
     description:
       "PoKeGrid empowers you to transform your power grid into a dynamic network fueled by the incredible skills of electric-type Pokémon. Picture Pikachu unleashing its signature Thunderbolt attack, channeling a surge of sustainable energy directly into your home's electrical system. Meanwhile, Electabuzz utilizes its electrifying Thunder Punch to supercharge your electric vehicle, providing a thrilling and eco-conscious mode of transportation.",
+    pokemons: "pokemons",
     toTheGrid: "to the grid",
   },
   es: {
@@ -26,15 +27,16 @@ export const dictionary: Dictionary = {
     shopNow: "Comprar ya!",
     players: "Jugadores",
     login: "Iniciar sesion",
-    cart: "Cart",
+    cart: "Carrito",
     //dropDown
-    myProfile: "My profile",
-    logout: "logout",
+    myProfile: "Mi perfil",
+    logout: "cerrar sesion",
     //home page
     welcome: "Bienvenido a PoKeGrid",
     greenEnergy: "Energía verde por pokemones",
     description:
       "PoKeGrid te permite transformar tu red eléctrica en una red dinámica alimentada por las increíbles habilidades de los Pokémon de tipo eléctrico. Imagina a Pikachu desatando su característico ataque Rayo, canalizando una oleada de energía sostenible directamente al sistema eléctrico de tu casa. Por su parte, Electabuzz utiliza su electrizante Golpe Trueno para sobrealimentar tu vehículo eléctrico, proporcionándote un medio de transporte emocionante y respetuoso con el medio ambiente.",
+    pokemons: "pokemones",
     toTheGrid: "ir a la red",
   },
   de: {
@@ -53,6 +55,7 @@ export const dictionary: Dictionary = {
     greenEnergy: "Grüne Energie von Pokemon",
     description:
       "PoKeGrid ermöglicht es dir, dein Stromnetz in ein dynamisches Netzwerk zu verwandeln, das durch die unglaublichen Fähigkeiten von Pokémon des Typs Elektro angetrieben wird. Stell dir vor, wie Pikachu seine charakteristische Attacke Donnerblitz entfesselt und einen Schwall nachhaltiger Energie direkt in das Stromnetz deines Hauses leitet. Electabuzz nutzt seinen elektrisierenden Donnerschlag, um dein Elektrofahrzeug aufzuladen und so ein aufregendes und umweltbewusstes Transportmittel zu schaffen.",
+    pokemons: "Pokemons",
     toTheGrid: "zum Stromnetz",
   },
 };
