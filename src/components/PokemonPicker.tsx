021f4196145534eebaf985294aabd9287a565014
santiagomanso <santiagomanso@web.de>
2023-08-27 18:28:44 +0200

    ✅ model grid / fixed pokemon img / grid interface

--- src/components/PokemonPicker.tsx
+++ src/components/PokemonPicker.tsx
@@ -19,10 +19,9 @@ const PokemonPicker = () => {
               dark:from-slate-500 dark:to-gray-800
               `}
             >
-              <Image
+              <img
                 src={pokemon.image}
                 alt={pokemon.name}
-                fill={true}
                 className={`w-full max-w-[130px] ${
                   user.coins >= pokemon.cost
                     ? "blur-none"
@@ -31,10 +30,9 @@ const PokemonPicker = () => {
               />
               {pokemon.cost > 10 && (
                 <>
-                  <Image
+                  <img
                     src="https://i.ibb.co/yyYY8pJ/lock.png"
                     alt={pokemon.name}
-                    fill={true}
                     className="absolute left-1/2 top-1/2   z-10 h-full w-full -translate-x-1/2 -translate-y-1/2 bg-black/30 p-10"
                   />
                   <span className="absolute right-0 top-0 z-10 rounded-full border-2 border-slate-400 bg-gradient-to-br from-yellow-200 to-amber-600 px-2 py-1 font-medium text-gray-600 dark:text-gray-700">
